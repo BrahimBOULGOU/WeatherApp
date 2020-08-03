9607104b0de4f85c46425c5a97cda6132759c46c
Brahim <brahimboulgou@gmail.com>
2020-08-03 23:00:51 +0200

    test 3

--- app/src/androidTest/java/com/exercice/weatherapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/exercice/weatherapp/ExampleInstrumentedTest.java
@@ -3,7 +3,8 @@ package com.exercice.weatherapp;
 import android.content.Context;
 
 import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +21,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = ApplicationProvider.getApplicationContext();
 
         assertEquals("com.exercice.weatherapp", appContext.getPackageName());
     }
